e791eef9860e3af539baa2d3b4d0f6bc158446eb
WadsonBrito <wadsonmelo@gmail.com>
2025-03-14 19:58:22 +0000

    opternario1

--- opternario2.c
+++ opternario2.c
@@ -1,1 +1,18 @@
+# include <stdio.h>
+int main(){
+    float temperatura; 
+    int resultado;
+    
 
+    printf("Digite a temperatura: \n");
+    scanf("%f", &temperatura);
+
+    resultado = temperatura > 32.0 ? 1 : 0;
+
+    if(resultado == 1){
+        printf("%.1f °C Temperatura Elevada\n", temperatura);
+    } else {
+        printf("%.1f °C Temperatura aceitavel\n", temperatura);
+    }
+    return 0;
+}
